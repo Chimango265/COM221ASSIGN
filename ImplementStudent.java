280475df3e3262e5c6ee2e10996830d89a3d715f
Chimango265 <61537595+Chimango265@users.noreply.github.com>
2020-10-20 17:49:43 +0200

    Update ImplementStudent.java
    I have changed the value for the index of student array object from 2 to 0 since the length of the array of object is 2 and having indexes 0 to 1 making 2 an index out of bounds and of course some comments for documentation.

--- ImplementStudent.java
+++ ImplementStudent.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 public class ImplementStudent{
 	public static void main(String[]args){
 		Scanner input = new Scanner(System.in);
-		Student []student = new Student[2];
+		Student []student = new Student[2];//creating an array of objects
 		for(int i = 0;i<student.length;i++){
 			student[i] = new Student();
-		}
+		}//the array of objects have been inialised
 		System.out.println("enter your full name");
 		String name = input.nextLine();
 		System.out.println("enter number of uour grades");
 		int numOfGrades = input.nextInt();
 		System.out.println("enter the grades");
 		double []grades = new double[numOfGrades];
+		//inputing the value for the grades array
 		for(int i = 0;i<grades.length;i++){
 			grades[i] = input.nextDouble();
 		}
-		student[2].setName(name);
-		student[2].setGrades(grades);
-		student[2].printStudentDetails();
+		//calling on methods for the student object with index 0
+		student[0].setName(name);
+		student[0].setGrades(grades);
+		student[0].printStudentDetails();
 
 	}
 }
